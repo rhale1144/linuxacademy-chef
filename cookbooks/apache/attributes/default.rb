46ab5b5f01575c26b4a5904b6f0112d9b4f31d80
root <root@rhale11441.mylabserver.com>
2016-09-02 22:38:56 +0000

    Updating our workstation chef repo

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -2,6 +2,7 @@ default["apache"]["sites"]["rhale11442"] = { "site_title" => "Rhale11442 website
 default["apache"]["sites"]["rhale11442b"] = { "site_title" => "Rhale11442b website coming soon", "port" => 80, "domain" => "rhale11442b.mylabserver.com" }
 default["apache"]["sites"]["rhale11444"] = { "site_title" => "Rhale11444 website", "port" => 80, "domain" => "rhale11444.mylabserver.com" }
 
+default["author"]["name"] = "Ryan"
 case node["platform"]
 when "centos"
 	default["apache"]["package"] = "httpd"
